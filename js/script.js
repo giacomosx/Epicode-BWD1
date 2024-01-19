63383582ec182b86ea5b6a2c0222e1b94f76c43b
giacomosx <giacomo.bartoli@me.com>
2024-01-19 11:59:35 +0100

    some updates

--- js/script.js
+++ js/script.js
@@ -5,53 +5,81 @@ const answersContainer = document.getElementById('answersContainer');
 const confirmBtn = document.getElementById('confirm');
 
 let timing = 90;
-timer.innerHTML = timing;
+let questionNumber = 0;
 
 let allAnswers = [];
+let userResults = []; // array's objects with all results (checkQuestion). it displays finals results ***
 
-let questionNumber = 0;
+let currentUserAnswer = ''; // global var for tracking the answer every time (takeBtnValue) ***
+let isCorrect = false;
+
 let totalPoints = 0;
 
-getQuestion();
-startTimer();
 
-function getQuestion() {
-    allAnswers.push(questions[questionNumber].correct_answer);
-    questions[questionNumber].incorrect_answers.forEach(answer => {
-      allAnswers.push(answer);
-    });
-    allAnswers.sort();
-    question.innerHTML = questions[questionNumber].question; 
-    getAnswers(allAnswers);
+window.onload = function () {
+  timer.innerHTML = timing;
+  startTest();
+  startTimer();
 }
 
-function getAnswers(array) {
+
+function startTest() {
+  question.innerHTML = questions[questionNumber].question;
+  getOptionsBtns(allAnswers);
+  // insert a function that takes the value of the pressed button
+}
+
+
+function getOptionsBtns(array) {
+  array.push(questions[questionNumber].correct_answer);
+  questions[questionNumber].incorrect_answers.forEach(answer => {
+    array.push(answer);
+  });
+
+  array.sort();
+
   const answersContainer = document.createElement('div');
   answersContainer.id = 'answersContainer';
   container.append(answersContainer);
+
   array.forEach(answer => {
-    const button = document.createElement('button'); 
+    const button = document.createElement('button');
     button.classList.add('answersBtn');
     button.innerHTML = answer;
     answersContainer.append(button);
   })
 }
 
+
+function nextQuestion() {
+  const answersContainer = document.getElementById('answersContainer');
+  answersContainer.remove();
+
+  questionNumber += 1;
+  timing = 91;
+  allAnswers = [];
+
+  if (questionNumber < questions.length) {
+    startTest();
+  } else {
+    // insert a function that shows final results
+  }
+}
+
+
 function startTimer() {
-  let countDown = setInterval(function() {
+  setInterval(function () {
     timing--;
     timer.innerHTML = timing;
-    if (timing === 0){
-      clearInterval(countDown);
+    if (timing === 0) {
+      // insert a function that checks if the value entered by the user is correct
+      nextQuestion();
     }
   }, 1000)
 }
 
-confirmBtn.addEventListener('click', function() {
-  const answersContainer = document.getElementById('answersContainer');
-  answersContainer.remove();
-  questionNumber += 1;
-  timing = 91;
-  allAnswers = [];
-  getQuestion();
+
+confirmBtn.addEventListener('click', function () {
+  // insert a function that checks if the value entered by the user is correct
+  nextQuestion();
 })
